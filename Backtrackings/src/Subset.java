e32be505845c369f9c59b0b774c4294846ad5a23
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-01 11:26:32 +0530

    backtracking

--- Backtrackings/src/Subset.java
+++ Backtrackings/src/Subset.java
@@ -2,11 +2,20 @@ public class Subset {
 
     public static void findSubsets(String str, String ans, int i){
 
+        if (i == str.length()){
+            if (ans.length() == 0){
+                System.out.println("null");
+            } else {
+                System.out.println(ans);
+            }
+            return;
+        }
 
-adsaa
+        findSubsets(str, ans+str.charAt(i), i+1);
+        findSubsets(str, ans, i+1);
     }
 
     public static void main(String[] args) {
-
+        findSubsets("abc","",0);
     }
 }
