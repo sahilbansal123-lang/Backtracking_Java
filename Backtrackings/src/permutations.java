c8f173c3dc4d05f20844f3be38e75834e6a912b2
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-01 12:02:07 +0530

    new

--- Backtrackings/src/permutations.java
+++ Backtrackings/src/permutations.java
@@ -1,10 +1,21 @@
 public class permutations {
 
-    public static void findPermutations(String str, String ans, int i){
+    public static void findPermutations(String str, String ans){
+
+        if (str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i+1);
+            findPermutations(newStr, ans+curr);
+        }
 
     }
 
     public static void main(String[] args) {
-
+        findPermutations("abc", "");
     }
 }
